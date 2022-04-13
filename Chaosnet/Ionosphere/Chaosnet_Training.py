28f0cccd49951b90b1eb52ec0d3a08574bfd6e44
Deeksha Sethi <63807125+deeksha-sethi03@users.noreply.github.com>
2022-04-13 20:16:32 +0530

    Add files via upload

---
+++ Chaosnet/Ionosphere/Chaosnet_Training.py
@@ -0,0 +1,90 @@
+"""
+Created on Thu Dec 2, 2021
+
+Author: Deeksha Sethi (deeksha.sethi03@gmail.com)
+Code Description: A python code to tune the hyperparameters of ChaosNet on the Ionosphere dataset.
+Dataset Source: https://archive.ics.uci.edu/ml/datasets/ionosphere
+
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from Codes import k_cross_validation
+
+
+'''
+_______________________________________________________________________________
+
+Rule used for renaming class labels: (Class Label - Numeric Code)
+_______________________________________________________________________________
+    b (Bad)      -     0
+    g (Good)     -     1
+_______________________________________________________________________________
+
+Variable description:
+_______________________________________________________________________________
+    ionosphere      -   Complete Ionosphere dataset.    
+    X               -   Data attributes.
+    y               -   Corresponding labels for X.
+    X_train         -   Data attributes for training (80% of the dataset).
+    y_train         -   Corresponding labels for X_train.
+    X_test          -   Data attributes for testing (20% of the dataset).
+    y_test          -   Corresponding labels for X_test.
+    X_train_norm    -   Normalizised training data attributes (X_train).
+    X_test_norm     -   Normalized testing data attributes (X_test).
+_______________________________________________________________________________
+
+CFX hyperparameter description:
+_______________________________________________________________________________
+    INITIAL_NEURAL_ACTIVITY         -   Initial Neural Activity.
+    EPSILON                         -   Noise Intensity.
+    DISCRIMINATION_THRESHOLD        -   Discrimination Threshold.
+    
+    Source: Harikrishnan N.B., Nithin Nagaraj,
+    When Noise meets Chaos: Stochastic Resonance in Neurochaos Learning,
+    Neural Networks, Volume 143, 2021, Pages 425-435, ISSN 0893-6080,
+    https://doi.org/10.1016/j.neunet.2021.06.025.
+    (https://www.sciencedirect.com/science/article/pii/S0893608021002574)
+_______________________________________________________________________________
+
+
+''' 
+
+
+#import the IONOSPHERE Dataset 
+ionosphere = np.array(pd.read_csv('ionosphere_data.txt', sep=",", header=None))
+
+
+#reading data and labels from the dataset
+X, y = ionosphere[:,range(0,ionosphere.shape[1]-1)], ionosphere[:,ionosphere.shape[1]-1].astype(str)
+
+
+#Norm: B -> 0;  G -> 1
+y = y.reshape(len(y),1)
+y = np.char.replace(y, 'b', '0', count=None)
+y = np.char.replace(y, 'g', '1', count=None)
+y = y.astype(int)
+
+
+#Splitting the dataset for training and testing (80-20)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+
+
+
+#Normalisation - Column-wise
+X_train_norm = X_train
+X_train_norm[:,range(2,X_train.shape[1])] = (X_train[:,range(2,X_train.shape[1])]-np.min(X_train[:,range(2,X_train.shape[1])],0))/(np.max(X_train[:,range(2,X_train.shape[1])],0)-np.min(X_train[:,range(2,X_train.shape[1])],0))
+X_train_norm = X_train_norm.astype(float)
+X_test_norm = X_test
+X_test_norm[:,range(2,X_test.shape[1])] = (X_test[:,range(2,X_test.shape[1])]-np.min(X_test[:,range(2,X_test.shape[1])],0))/(np.max(X_test[:,range(2,X_test.shape[1])],0)-np.min(X_test[:,range(2,X_test.shape[1])],0))
+X_test_norm = X_test_norm.astype(float)
+
+
+
+#validation
+FOLD_NO = 5
+INITIAL_NEURAL_ACTIVITY = np.arange(0.01, 0.99, 0.01)
+DISCRIMINATION_THRESHOLD = [0.499, 0.969]
+EPSILON = np.arange(0.001, 0.5000, 0.001)
+k_cross_validation(FOLD_NO, X_train_norm, y_train, X_test_norm, y_test, INITIAL_NEURAL_ACTIVITY, DISCRIMINATION_THRESHOLD, EPSILON)
