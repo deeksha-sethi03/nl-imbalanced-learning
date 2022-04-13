1c6c49b64be055ae6534bf5aec86546a7a689db9
Deeksha Sethi <63807125+deeksha-sethi03@users.noreply.github.com>
2022-04-13 19:46:40 +0530

    Add files via upload

---
+++ Gaussian Naive Bayes/Wine/CFX_GaussianNB_Testing.py
@@ -0,0 +1,113 @@
+"""
+Created on Thu Dec 2, 2021
+
+Author: Deeksha Sethi (deeksha.sethi03@gmail.com)
+Code Description: A python code to test the efficacy of CFX+Gaussian Naive Bayes on the Wine dataset.
+Dataset Source: https://archive.ics.uci.edu/ml/datasets/wine
+
+"""
+
+import os
+import numpy as np
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+import ChaosFEX.feature_extractor as CFX
+
+'''
+_______________________________________________________________________________
+
+Rule used for renaming class labels: (Class Label - Numeric Code)
+_______________________________________________________________________________
+    1.0       -     0
+    2.0       -     1
+    3.0       -     2
+_______________________________________________________________________________
+
+Variable description:
+_______________________________________________________________________________
+    wine            -   Complete Wine dataset.    
+    X               -   Data attributes.
+    y               -   Corresponding labels for X.
+    X_train         -   Data attributes for training (80% of the dataset).
+    y_train         -   Corresponding labels for X_train.
+    X_test          -   Data attributes for testing (20% of the dataset).
+    y_test          -   Corresponding labels for X_test.
+    X_train_norm    -   Normalizised training data attributes (X_train).
+    X_test_norm     -   Normalized testing data attributes (X_test).
+_______________________________________________________________________________
+
+CFX hyperparameter description:
+_______________________________________________________________________________
+    INA         -   Initial Neural Activity
+    EPSILON_1   -   Noise Intensity
+    DT          -   Discrimination Threshold
+        
+    Source: Harikrishnan N.B., Nithin Nagaraj,
+    When Noise meets Chaos: Stochastic Resonance in Neurochaos Learning,
+    Neural Networks, Volume 143, 2021, Pages 425-435, ISSN 0893-6080,
+    https://doi.org/10.1016/j.neunet.2021.06.025.
+    (https://www.sciencedirect.com/science/article/pii/S0893608021002574)
+
+_______________________________________________________________________________
+
+Performance metric used:
+_______________________________________________________________________________
+    Macro F1-score (F1SCORE/ f1) -
+    The F1 score can be interpreted as a harmonic mean of the precision and 
+    recall, where an F1 score reaches its best value at 1 and worst score at 
+    0. The relative contribution of precision and recall to the F1 score are 
+    equal; 'macro' calculates metrics for each label, and find stheir 
+    unweighted mean. This does not take label imbalance into account.
+    Source: Scikit Learn 
+    (https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html)
+_______________________________________________________________________________
+
+'''    
+
+
+#import the WINE Dataset 
+wine = np.array(pd.read_csv('wine_data.txt', sep=",", header=None))
+
+
+#reading data and labels from the dataset
+X, y = wine[:,range(1,wine.shape[1])], wine[:,0]
+y = y.reshape(len(y),1)
+y[y==1.0]=0.0
+y[y==2.0]=1.0
+y[y==3.0]=2.0
+
+
+#Splitting the dataset for training and testing (80-20)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+
+
+#Normalisation - Column-wise
+X_train_norm = (X_train - np.min(X_train,0)) / (np.max(X_train,0) - np.min(X_train,0))
+X_train_norm = X_train_norm.astype(float)
+X_test_norm = (X_test - np.min(X_test,0)) / (np.max(X_test,0) - np.min(X_test,0))
+X_test_norm = X_test_norm.astype(float)
+
+
+#Testing
+PATH = os.getcwd()
+RESULT_PATH = PATH + '/CFX-TUNING/RESULTS/' 
+    
+INA = np.load(RESULT_PATH+"/h_Q.npy")[0]
+EPSILON_1 = np.load(RESULT_PATH+"/h_EPS.npy")[0]
+DT = np.load(RESULT_PATH+"/h_B.npy")[0]
+
+FEATURE_MATRIX_TRAIN = CFX.transform(X_train_norm, INA, 10000, EPSILON_1, DT)
+FEATURE_MATRIX_VAL = CFX.transform(X_test_norm, INA, 10000, EPSILON_1, DT)            
+
+clf = GaussianNB()
+clf.fit(FEATURE_MATRIX_TRAIN, y_train.ravel())
+
+y_pred = clf.predict(FEATURE_MATRIX_VAL)
+f1 = f1_score(y_test, y_pred, average='macro')
+print('TESTING F1 SCORE', f1)
+
+np.save(RESULT_PATH+"/F1SCORE_TEST.npy", np.array([f1]) )
+
+#TESTING F1 SCORE 0.9761600681140911
